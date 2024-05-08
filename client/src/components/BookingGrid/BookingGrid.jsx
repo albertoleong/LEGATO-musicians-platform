9aeab99b6de8adc6e3884d9ba0ad75d95eb3d945
Alberto Leong <117922475+albertoleong@users.noreply.github.com>
2024-05-07 23:51:49 -0400

    artist filtering feature

--- client/src/components/BookingGrid/BookingGrid.jsx
+++ client/src/components/BookingGrid/BookingGrid.jsx
@@ -3,31 +3,49 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const BookingGrid = () => {
-
     const [artistsList, setArtistsList] = useState([])
+    const [filter, setFilter] = useState('')
     const API_URL = "http://localhost:8080/"
-    //const API_URL = process.env.REACT_APP_API_URL
 
     useEffect(() => {
         const getArtists = async() => {
-        try {
-            const artists = await axios.get(API_URL)
-            setArtistsList(artists.data)
-            console.log(artists.data)
-        } catch (error) {
-            console.error(error)
+            try {
+                const artists = await axios.get(API_URL)
+                setArtistsList(artists.data)
+            } catch (error) {
+                console.error(error)
             }
         }
         getArtists()
     },[] )
 
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+    }
+
+    const filteredArtists = artistsList.filter(artist => {
+        return artist.type.toLowerCase().includes(filter.toLowerCase()) || 
+               artist.music_styles.toLowerCase().includes(filter.toLowerCase())
+    })
+
     return (
         <section className='grid'>
+            <input 
+                className='grid__input'
+                type="text" 
+                placeholder="Filter by type or music genre" 
+                value={filter} 
+                onChange={handleFilterChange} 
+            />
             <ul className='grid__list'>
-                {artistsList.map((artist) => {
+                {filteredArtists.map((artist) => {
                     return (
-                        <li className='grid__item'>
-                            <h2>{artist.name}</h2>
+                        <li className='grid__item' key={artist.id}>
+                            <div className='grid__div'>
+                                <h2 className='grid__name'>{artist.name}</h2>
+                                <p>{artist.type}</p>
+                            </div>
+                            <p className='grid__par'>{artist.music_styles}</p>
                         </li>
                     )
                 })} 
@@ -35,4 +53,5 @@ const BookingGrid = () => {
         </section>
     )
 }
+
 export default BookingGrid
--- client/src/pages/Booking/Booking.jsx
+++ client/src/pages/Booking/Booking.jsx
@@ -2,10 +2,11 @@ import BookingGrid from '../../components/BookingGrid/BookingGrid'
 import './Booking.scss'
 
 const Booking = () => {
+    
     return (
-        <main>
+        <main className='booking'>
             <section className='booking__topSection'>
-                <h1 className='booking__title'>Book the musician</h1>
+                <h1 className='booking__title'>Book your musician</h1>
             </section>
             <BookingGrid />
         </main>
