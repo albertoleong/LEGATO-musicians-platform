ff1cf53d953d8da52c60ad96ec359bf6ffe11ae7
Alberto Leong <117922475+albertoleong@users.noreply.github.com>
2024-05-09 16:49:06 -0400

    bcrypt and patch route

--- server/routes/artistsRoutes.js
+++ server/routes/artistsRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 import knex from 'knex';
 import knexfile from '../knexfile.js';
 const db = knex(knexfile.development);
-
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 router.route('/')
     .get(async (req, res) => {
@@ -27,6 +28,7 @@ router.route('/')
         if (!name || !type || !music_styles || !instruments || !location || !description || !email ) 
         {return res.status(400).json({ error: 'Missing required fields in request, please check' })}
         
+        const hashedPassword = bcrypt.hashSync(password);
         try {
             const [newArtistId] = await db('artists').insert({
                 name,
@@ -35,7 +37,8 @@ router.route('/')
                 instruments,
                 location,
                 description,
-                email
+                email,
+                password:hashedPassword
             });
             const newArtist = await db('artists').where({ id: newArtistId }).first();
             return res.status(201).json(newArtist);
@@ -44,6 +47,32 @@ router.route('/')
             return res.status(500).json({ error: 'Error' });
         }        
     })
+
+router.route('/login')
+    .post(async (req, res) => {
+       
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).send("PLease enter ther required fields");
+        }
+        const user = await db("artists").where({ email: email }).first();
+        if (!user) {
+            return res.status(400).send("Invalid email");
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password);
+
+        if (!isPasswordCorrect) {
+            return res.status(400).send("Invalid password");
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_KEY,
+        );
+        res.json({ token: token });
+        res.status(200);
+    });
 
 
 router.route('/:id')
@@ -69,5 +98,40 @@ router.route('/:id')
             res.status(404).json({message:err})
         }
     })
+
+    .patch(async (req, res) => {
+        const { id } = req.params;
+        const {
+            name,
+            type,
+            music_styles,
+            instruments,
+            location,
+            description,
+            email
+        } = req.body;
+        try {
+            const updatedArtist = await db('artists')
+                .where({ id })
+                .update({
+                    name,
+                    type,
+                    music_styles,
+                    instruments,
+                    location,
+                    description,
+                    email
+                });
+    
+            if (!updatedArtist) {
+                return res.status(404).json({ error: 'Artist not found' });
+            }
+    
+            res.json({ message: 'Artist updated successfully' });
+        } catch (err) {
+            res.status(500).json({ error: 'Failed to update artist' });
+        }
+    });
+    
 
 export default router
